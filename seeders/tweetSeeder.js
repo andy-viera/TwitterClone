79840a05a0aafa358ee3c0ee7e23a73513abfd38
Andy Viera <79466971+andy-viera@users.noreply.github.com>
2023-02-23 10:47:34 -0300

    v0.3

--- seeders/tweetSeeder.js
+++ seeders/tweetSeeder.js
@@ -8,12 +8,19 @@ module.exports = async () => {
   const tweets = [];
 
   for (let i = 0; i < 100; i++) {
+    const randomUserlikesArray = [];
+    const randomLikesAmount = faker.datatype.number({ min: 0, max: 10 });
+    for (let i = 0; i < randomLikesAmount; i++) {
+      let randomUserlike = await User.findOne().skip(faker.datatype.number({ min: 0, max: 9 }));
+      randomUserlikesArray.push(randomUserlike);
+    }
+
     const randomUser = await User.findOne().skip(faker.datatype.number({ min: 0, max: 9 }));
 
     const tweet = new Tweet({
       content: faker.lorem.paragraph({ min: 1, max: 4 }),
       author: randomUser,
-      likes: randomUser,
+      likes: randomUserlikesArray,
     });
 
     tweets.push(tweet);
